b0b32819999431a2b65cf83dc948d9d3f1feae6c
zfilm <morten.foyn.boe@gmail.com>
2015-08-06 00:05:47 +0200

    Init

---
+++ Nolo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nolo.Startup))]
+namespace Nolo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
