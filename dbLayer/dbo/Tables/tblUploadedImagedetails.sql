3e986c7fd3db235d94e92abc4f3ae61eab612820
zfilm <morten.foyn.boe@gmail.com>
2015-08-06 00:28:17 +0200

    Added connection to my test db.

---
+++ dbLayer/dbo/Tables/tblUploadedImagedetails.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[tblUploadedImagedetails] (
+    [ImageID]      INT           IDENTITY (1, 1) NOT NULL,
+    [ImageName]    VARCHAR (100) NOT NULL,
+    [ImageContent] IMAGE         NOT NULL,
+    [Createdby]    VARCHAR (100) NOT NULL,
+    [CreatedDt]    DATETIME      NOT NULL,
+    [Updatedby]    VARCHAR (100) NULL,
+    [UpdatedDt]    DATETIME      NULL,
+    [Active]       BIT           NOT NULL
+);
+
