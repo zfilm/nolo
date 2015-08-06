7dfea60ec26e1e5bb7d7c1fb0563414f67b413e6
zfilm <morten.foyn.boe@gmail.com>
2015-08-06 23:30:32 +0200

    #1 Added Db Layer

--- dbLayer/dbo/Tables/persons.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[persons] (
-    [id]         INT           IDENTITY (1, 1) NOT NULL,
-    [last_name]  VARCHAR (255) NOT NULL,
-    [first_name] VARCHAR (255) NULL,
-    [address]    VARCHAR (255) NULL,
-    [city]       VARCHAR (255) NULL,
-    PRIMARY KEY CLUSTERED ([id] ASC)
-);
-
